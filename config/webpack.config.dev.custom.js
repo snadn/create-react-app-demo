999adcde6cd2484f4ac7c1d933879f72800ca84b
snadn <snadn@snadn.cn>
2017-04-25 11:05:10 +0800

    update

--- config/webpack.config.dev.custom.js
+++ config/webpack.config.dev.custom.js
@@ -1,4 +1,5 @@
-var config = require('react-scripts/config/webpack.config.dev.default');
+// var config = require('react-scripts/config/webpack.config.dev.default');
+var config = require('./webpack.config.dev.default');
 
 config.resolve.extensions = ['.web.js', '.js', '.json', '.jsx', ''];
 config.module.loaders.find(item => item.loader === 'babel').query.babelrc = true;
